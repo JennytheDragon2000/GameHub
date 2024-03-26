552d1a4cf9a3c7b62a031e3ab55b1d1893eb289e
jenny <www.srinath.25.com@gmail.com>
2024-03-25 21:03:29 -0400

    Fixed one small bux

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuIcon,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  onSelectPlatform: (Platform: Platform) => void;
+  selectedPlatfrom: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatfrom }: Props) => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatfrom?.name || "Platforms"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
